772ca59e3b03487c3d8f23078a7fc952ec5bbf80
Ocheben <ocheben@gmail.com>
2019-04-02 09:41:47 +0100

    first deployment

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const mockDB = require('./mockDB')
 
 const express = require('express')
 const app = express();
+app.use(express.json())
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -9,6 +10,15 @@ app.get('/', (req, res) => {
 
 app.get('/auth', (req, res) => {
     res.json(mockDB.users)
+});
+
+app.post('/auth', (req, res) => {
+    for (let i = 0; i < mockDB.users.length; i++) {
+        while (mockDB.users[i].email===req.body.email &&
+            mockDB.users[i].password === req.body.password) {
+            res.json(mockDB.users[i])
+        }
+    }
 });
 
 app.listen(8000, () => {
--- mockDB.js
+++ mockDB.js
@@ -1,9 +1,13 @@
 const users = [
     {
+        email: "admin@ce.com",
+        password:"admin",
         name: "Albert Einstien",
         role: "admin"
     },
     {
+        email: "biker1@ce.com",
+        password:"biker1",
         name: "Niccolò Machiavell",
         role: "biker"
     }
